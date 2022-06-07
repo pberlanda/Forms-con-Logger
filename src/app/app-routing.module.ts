71a84352e0f699a847e68b4ba085bc22363909e0
pberlanda <paolo.berlanda@marconirovereto.it>
2022-06-07 18:41:23 +0200

    aggiunte routes nell app-routing. Commit prima di modificare app-component con il routing. In bocca al lupo!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { RouterModule, Routes } from '@angular/router';
+import { MyComponent } from './my/my.component';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {path: 'my', component: MyComponent}
+  {path: 'app', component: AppComponent}
+];
+
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports:[ RouterModule ]
 })
 export class AppRoutingModule { }
